dc34596baca924c1a5094f88ae03f09c5717fc3e
cchompoottr <chompoo99075@gmail.com>
2024-04-23 19:27:25 +0700

    finish log-in page

--- pages/login.js
+++ pages/login.js
@@ -5,9 +5,30 @@ export default function Login() {
         <div className={`bg-cover bg-center flex justify-center items-center ${styles.container}`}>
             <div className={styles.flexContainer}>
                 <div className={styles.Box} style={{ display: 'flex',flexDirection: 'column'}}>
-                    <img src="/logo.jpg" alt="Logo" width="200" height="100"  className="logo" />
-                    <h1 className={styles.head}>LogIn</h1>
-                    <p className={styles.sub}>Please enter your username and password to log in.</p>
+                    <img src="/logo.jpg" alt="Logo" width="270" height="150"  className="logo" />
+                    <h1 className={styles.head}>Log In</h1>
+                    <p className={styles.sub}>Please enter your username and password to log in and <br></br>access our online candle shop.</p>
+
+                    <form className={styles.userBox}>
+                        <input
+                            name="username"
+                            placeholder="  Username"
+                            className={styles.textName}
+                        />
+                    </form>
+
+                    <form className={styles.passwordBox}>
+                        <input
+                            name="password"
+                            placeholder="  Password"
+                            className={styles.textName}
+                        />
+                    </form>
+
+                    <a href="/">
+                        <button className={styles.payButton}>Login</button>
+                    </a>
+
                 </div>
             </div>
         </div>
