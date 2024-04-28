8f29b3311c53bace603de422a3cdb233ea46850f
cchompoottr <chompoo99075@gmail.com>
2024-04-28 16:16:16 +0700

    edit about page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Footer from "@/components/Footer";
+
 import Navbar from "@/components/Navbar";
 import styles from '../styles/about.module.css';
 
@@ -11,39 +11,39 @@ export default function about(){
             </Head>
             <Navbar />
 
+        <div className={styles.flexcontainer}>
             <div className={styles.container}>            
-                <div className={styles.left}>
-                    <img src="/about/login.jpg" alt="image" width="400" height="350"  className="image" />
-                </div>
 
-                <div className={styles.right}>
-                    <h1 className={styles.head}>About</h1>
-                    <h3 className={styles.sub}>"Discover the magic of soy wax, known for its clean burn and long-lasting fragrance,<br></br>  
-                                                ensuring that every candle from our collection is a delightful experience from start<br></br>  
-                                                to finish."</h3>
+                    <h1 className={styles.head}>Nature Candle</h1>
+                    <h3 className={styles.sub}>"Discover the magic of soy wax, known for its clean burn and long-lasting fragrance, ensuring<br></br>  
+                                                that every candle from our collection is a delightful experience from start to finish."</h3>
 
                     <a href="/products">
                         <button className={styles.button}>Shop Now</button>
                     </a>
-                </div>
             </div>
+                
+           
                 
                 
                 {/* เพิ่ม flexcontainer ด้านล่าง */}
-                <div className={styles.flexcontainer}>
-                <h1 className={styles.values}>My values</h1>
-                    <div className={styles.containerBox}>
-                        <div className={styles.box}>
-                            <h1 className={styles.num}>1</h1>
+
+                    <div className={styles.wrapper}>
+                        <div className={styles.boxleft}>
+
+                        <img className={styles.image}src="/about/about1.jpg" alt="About Image"></img>
+
                             <p className={styles.title}>Integrity and Transparency</p>
-                            <p className={styles.content}> We carefully select high-quality<br></br> 
-                                ingredients and utilize cutting-edge<br></br> 
-                                technology to provide you with<br></br> 
-                                premium-quality and transparently<br></br> 
-                                sourced candles.</p>
+                            <p className={styles.content}> We carefully select high-quality ingredients<br></br> 
+                                and utilize cutting-edge technology to<br></br> 
+                                provide you with premium-quality and<br></br> 
+                                transparentlysourced candles.
+                            </p>
                         </div>
-                        <div className={styles.box}>
-                            <h1 className={styles.num}>2</h1>
+
+
+                        <div className={styles.boxmiddle}>
+                            <img className={styles.image}src="/about/about2.jpg" alt="About Image"></img>
                                 <p className={styles.title}>Quality Craftsmanship</p>
                                 <p className={styles.content}>Each candle is hand-poured with<br></br> 
                                     meticulous attention to detail, ensuring<br></br> 
@@ -51,8 +51,8 @@ export default function about(){
                                     quality and craftsmanship.</p>
                         </div>
                         
-                        <div className={styles.box}>
-                            <h1 className={styles.num}>3</h1>
+                        <div className={styles.boxright}>
+                            <img className={styles.image}src="/about/about3.jpg" alt="About Image"></img>
                                 <p className={styles.title}>Environmental Responsibility</p>
                                 <p className={styles.content}>We are committed to sustainability<br></br> 
                                     and minimize our environmental impact<br></br> 
@@ -61,9 +61,16 @@ export default function about(){
                         </div>
 
                     </div>
-                </div>
+        
 
-            <Footer/>
+                    </div>
+
+                 
+
+                
+               
+
+
         </>
     )
 }
